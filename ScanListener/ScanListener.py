dc91bb7cb582a9d97afb7a8c0a2b19a7bdbf47a9
Nick <runningman260@users.noreply.github.com>
2024-08-08 13:59:38 -0400

    initial commit for scan listener

---
+++ ScanListener/ScanListener.py
@@ -0,0 +1,129 @@
+#/bin/python3
+
+#######################################################################################
+#                                                                                     #
+#  Task to read in registered cars.                                                   #
+#  Car info is pushed to a database table to collect the input for processing.        #
+#                                                                                     #
+############################################################ Pittsburgh Shootout LLC ##
+
+from config import Config
+import time
+import atexit
+from database_helper import *
+import paho.mqtt.client as paho
+import json
+import datetime
+
+def exit_handler():
+	print(' Cleaning Up!')
+	client.loop_stop()
+	exit(1)
+
+def environment_check():
+	## Check if table exists
+	## If EXISTS, clear rows if desired
+	## If !EXISTS, create
+
+	delete_if_exists = False
+
+	for table_name in [startline_table_name, finishline_table_name]:
+		print("CHECKING if " + table_name + " EXISTS")
+		if(check_table_exist(table_name)):
+			print(table_name + " EXISTS")
+			if(delete_if_exists):
+				print("DELETING ROWS")
+				delete_all_table_rows(table_name)
+			else:
+				print("NOT DELETING ROWS")
+		else:
+			print("CREATING " + table_name)
+			sql_cmd ="""
+				CREATE TABLE {table_name}(
+					id SERIAL PRIMARY KEY,
+					scan_number VARCHAR(255),
+					tag_number  VARCHAR(255),
+					created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+					updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+				""".format(table_name=table_name)
+			if(table_name == startline_table_name ):
+				sql_cmd = sql_cmd + """
+					,created_by  VARCHAR(255)
+				"""
+			sql_cmd = sql_cmd + ")"
+			create_table(sql_cmd)
+
+		## Check if function and trigger are present. If not, create
+		function_name = table_name + "_trigger_set_timestamp"
+		trigger_name = table_name + "_set_timestamp"
+		if(not check_function_exists(function_name) and not check_trigger_exists(trigger_name)):
+			print("Function or trigger for " + table_name + " do not exist, creating...")
+			create_timestamp_function(function_name)
+			create_timestamp_trigger(table_name, function_name, trigger_name)
+	return True
+
+def create_mqtt_connection():
+	def on_connect(client, userdata, flags, reason_code, properties):
+		if reason_code == 0:
+			print("MQTT Client Connected")
+		else:
+			print("MQTT Client NOT Connected, rc= " + str(reason_code))
+		client.subscribe("/timing/slscan/newscan") #This goes here to sub on reconnection
+		client.subscribe("/timing/flscan/newscan") #This goes here to sub on reconnection
+		client.subscribe("/timing/webui/override") #This goes here to sub on reconnection
+	client = paho.Client(paho.CallbackAPIVersion.VERSION2,client_id=client_id)
+	client.username_pw_set(Config.MQTTUSERNAME, Config.MQTTPASSWORD)
+	client.on_connect = on_connect
+	client.connect(Config.MQTTBROKER, Config.MQTTPORT)
+	return client
+
+def sub_handler(client, userdata, msg):
+	#print(f"{msg.topic}: {msg.payload.decode()}")
+	if(msg.topic == "/timing/slscan/newscan"):
+		decoded_message = str(msg.payload.decode("utf-8"))
+		json_message = json.loads(decoded_message)
+		inserted = insert_newscan(startline_table_name, json_message["tag_number"], scan_number=json_message["scan_number"], created_by="SCAN")
+		print("Inserted Scan: " + str(json_message["scan_number"]) + " " + str(json_message["tag_number"]))
+
+	if(msg.topic == "/timing/flscan/newscan"):
+		decoded_message = str(msg.payload.decode("utf-8"))
+		json_message = json.loads(decoded_message)
+		inserted = insert_newscan(finishline_table_name, json_message["tag_number"], scan_number=json_message["scan_number"])
+		print("Inserted Scan: " + str(json_message["scan_number"]) + " " + str(json_message["tag_number"]))
+
+	if(msg.topic == "/timing/webui/override"):
+		decoded_message = str(msg.payload.decode("utf-8"))
+		json_message = json.loads(decoded_message)
+		inserted = insert_newscan(startline_table_name, json_message["tag_number"],scan_number="" ,created_by="ui_override")
+		print("Inserted Scan: " + "ui_override" + " " + str(json_message["tag_number"]))
+
+	client.publish("/timing/scanlistener/confirm_insert",build_payload((inserted>0),str(json_message["tag_number"]),msg.topic,str(datetime.datetime.now())))
+
+def build_payload(success, tag_number, source, timestamp):
+	payload = {"success":success,"tag":tag_number,"source":source,"created_at":timestamp}
+	return json.dumps(payload)
+
+if __name__ == '__main__':
+	startline_table_name = "startlinescan"
+	finishline_table_name = "finishlinescan"
+
+	if(not environment_check()):
+		print("Database Schema not correct. Exiting.")
+		atexit.register(exit_handler)
+	
+	client_id = Config.MQTTCLIENTID
+	client = create_mqtt_connection()
+	client.on_message = sub_handler
+	client.loop_start()
+	prev_health_check = 0
+	health_check_interval = 30 #seconds
+
+	try:
+		while True:
+			if((time.time() - prev_health_check) > health_check_interval):
+				client.publish("/timing/scanlistener/healthcheck",str(datetime.datetime.now()))
+				prev_health_check = time.time()
+	except KeyboardInterrupt:
+		pass
+	finally:	 
+		atexit.register(exit_handler)
---
+++ ScanListener/database_helper.py
@@ -0,0 +1,275 @@
+#/bin/python3
+
+#######################################################################################
+#                                                                                     #
+#  Helper Function to interact with a postgres database                               #
+#                                                                                     #
+############################################################ Pittsburgh Shootout LLC ##
+
+from config import Config
+import time
+import psycopg2
+
+def create_table(command):
+    try:
+        with psycopg2.connect(**Config.DATABASE) as conn:
+            with conn.cursor() as cur:
+                # execute the CREATE TABLE statement
+                cur.execute(command)
+    except (psycopg2.DatabaseError, Exception) as error:
+        print(error)
+
+def check_table_exist(table_name):
+    exists = False
+    command = """
+		SELECT EXISTS (
+			SELECT FROM information_schema.tables 
+			WHERE table_schema = current_schema() 
+			AND table_name = '{table_name}'
+			);""".format(table_name=table_name)
+    try:
+        with psycopg2.connect(**Config.DATABASE) as conn:
+            with conn.cursor() as cur:
+                # execute the CREATE TABLE statement
+                cur.execute(command)
+                exists = bool(cur.fetchone()[0])
+    except (psycopg2.DatabaseError, Exception) as error:
+        print(error)
+    return exists
+
+def delete_table(table_name):
+    sql = "DROP TABLE IF EXISTS " + table_name + " CASCADE;"
+    try:
+        with psycopg2.connect(**Config.DATABASE) as conn:
+            with conn.cursor() as cur:
+                # execute the DROP TABLE statement
+                cur.execute(sql)
+    except (psycopg2.DatabaseError, Exception) as error:
+        print(error)
+
+def insert_newscan(table_name, tag_number, scan_number=None, created_by=None):
+    if(created_by == None):
+        sql = """
+            INSERT INTO {table_name}(tag_number, scan_number) 
+            VALUES('{tag_number}', '{scan_number}') 
+            RETURNING id;""".format(table_name=table_name, tag_number=tag_number, scan_number=scan_number)
+    else:
+        sql = """
+            INSERT INTO {table_name}(tag_number, created_by, scan_number) 
+            VALUES('{tag_number}', '{created_by}', '{scan_number}') 
+            RETURNING id;""".format(table_name=table_name, tag_number=tag_number, scan_number=scan_number, created_by=created_by)
+    id = None
+    try:
+        with  psycopg2.connect(**Config.DATABASE) as conn:
+            with  conn.cursor() as cur:
+                # execute the INSERT statement
+                cur.execute(sql)
+
+                # get the generated id back
+                rows = cur.fetchone()
+                if rows:
+                    id = rows[0]
+
+                # commit the changes to the database
+                conn.commit()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        return id
+
+def insert_newcar(table_name, scan_time, tag_number, car_number, team_name):
+    sql = """
+        INSERT INTO {table_name}(scan_time, tag_number, car_number, team_name) 
+        VALUES('{scan_time}', '{tag_number}', '{car_number}', '{team_name}') 
+        RETURNING id;""".format(table_name=table_name, scan_time=scan_time, tag_number=tag_number, car_number=car_number, team_name=team_name)
+
+    id = None
+
+    try:
+        with  psycopg2.connect(**Config.DATABASE) as conn:
+            with  conn.cursor() as cur:
+                # execute the INSERT statement
+                cur.execute(sql)
+
+                # get the generated id back
+                rows = cur.fetchone()
+                if rows:
+                    id = rows[0]
+
+                # commit the changes to the database
+                conn.commit()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        return id
+
+def update_newcar(table_name, scan_time, tag_number, car_number=None, team_name=None, id=None):
+    if(id != None):
+        sql = """
+            UPDATE {table_name} 
+            SET tag_number = '{tag_number}', scan_time = '{scan_time}' 
+            WHERE id = {id} 
+            RETURNING id;
+            """.format(table_name=table_name, scan_time=scan_time, tag_number=tag_number, id=id)
+
+    elif(car_number != None):
+        sql = """        
+            UPDATE {table_name} 
+            SET tag_number = '{tag_number}', scan_time = '{scan_time}' 
+            WHERE car_number = '{car_number}'
+            RETURNING id;
+            """.format(table_name=table_name, scan_time=scan_time, tag_number=tag_number, car_number=car_number)
+    elif(team_name != None):
+        sql = """
+            UPDATE {table_name} 
+            SET tag_number = '{tag_number}', scan_time = '{scan_time}' 
+            WHERE team_name = '{team_name}'
+            RETURNING id;
+            """.format(table_name=table_name, scan_time=scan_time, tag_number=tag_number, team_name=team_name)
+    else:
+        return -1
+
+    return_id = None
+    try:
+        with  psycopg2.connect(**Config.DATABASE) as conn:
+            with  conn.cursor() as cur:
+                # execute the INSERT statement
+                cur.execute(sql)
+
+                # get the generated id back
+                rows = cur.fetchone()
+                if rows:
+                    return_id = rows[0]
+
+                # commit the changes to the database
+                conn.commit()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        return return_id
+
+def merge_car(table_name, scan_time, tag_number, car_number, team_name):
+    sql = """
+        MERGE INTO carreg w
+        USING (values('{scan_time}','{tag_number}','{car_number}','{team_name}')) as v
+        ON v.column3 = w.car_number
+        WHEN NOT MATCHED THEN
+        INSERT (scan_time, tag_number, car_number, team_name)
+        values(v.column1,v.column2,v.column3,v.column4)
+        WHEN MATCHED THEN
+        UPDATE SET tag_number = v.column2;
+        """.format(table_name=table_name, scan_time=scan_time, tag_number=tag_number, car_number=car_number, team_name=team_name)
+    id = None
+
+    try:
+        with  psycopg2.connect(**Config.DATABASE) as conn:
+            with  conn.cursor() as cur:
+                # execute the INSERT statement
+                cur.execute(sql)
+
+                # get the generated id back
+                rows = cur.fetchone()
+                if rows:
+                    id = rows[0]
+
+                # commit the changes to the database
+                conn.commit()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        return id
+
+def upsert_car(table_name, scan_time, tag_number, car_number, team_name):
+    sql = """
+        INSERT INTO carreg(scan_time, tag_number, car_number, team_name)
+        values('{scan_time}','{tag_number}','{car_number}','{team_name}')
+        ON CONFLICT(car_number)
+        DO UPDATE SET
+        tag_number = EXCLUDED.tag_number,
+        scan_time = EXCLUDED.scan_time
+        RETURNING (xmax = 0) AS inserted;
+        """.format(table_name=table_name, scan_time=scan_time, tag_number=tag_number, car_number=car_number, team_name=team_name)
+    inserted_flag = None
+
+    try:
+        with  psycopg2.connect(**Config.DATABASE) as conn:
+            with  conn.cursor() as cur:
+                # execute the INSERT statement
+                cur.execute(sql)
+
+                # get the generated inserted_flag back
+                rows = cur.fetchone()
+                if rows:
+                    inserted_flag = rows[0]
+
+                # commit the changes to the database
+                conn.commit()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        return inserted_flag
+    
+def delete_all_table_rows(table_name):
+    sql = "TRUNCATE " + table_name + ";"
+    try:
+        with psycopg2.connect(**Config.DATABASE) as conn:
+            with conn.cursor() as cur:
+                # execute
+                cur.execute(sql)
+    except (psycopg2.DatabaseError, Exception) as error:
+        print(error)
+
+def create_timestamp_function(function_name):
+    sql = """create or replace function {function_name}() 
+    returns trigger as $$
+    begin 
+        NEW.updated_at = NOW();
+        return new; 
+    end; 
+    $$ language plpgsql;""".format(function_name=function_name)
+    try:
+        with psycopg2.connect(**Config.DATABASE) as conn:
+            with conn.cursor() as cur:
+                # execute
+                cur.execute(sql)
+    except (psycopg2.DatabaseError, Exception) as error:
+        print(error)
+          
+def create_timestamp_trigger(table_name, function_name, trigger_name):
+    sql = """
+        CREATE TRIGGER "{trigger_name}" 
+        BEFORE UPDATE ON "{table_name}"
+        FOR EACH ROW EXECUTE PROCEDURE {function_name}();""".format(function_name=function_name, trigger_name=trigger_name, table_name=table_name)
+    try:
+        with psycopg2.connect(**Config.DATABASE) as conn:
+            with conn.cursor() as cur:
+                # execute
+                cur.execute(sql)
+    except (psycopg2.DatabaseError, Exception) as error:
+        print(error)
+
+def check_function_exists(function_name):
+    exists = False
+    command = """select exists(select * from pg_proc where proname = '{function_name}');""".format(function_name=function_name)
+    try:
+        with psycopg2.connect(**Config.DATABASE) as conn:
+            with conn.cursor() as cur:
+                # execute the CREATE TABLE statement
+                cur.execute(command)
+                exists = bool(cur.fetchone()[0])
+    except (psycopg2.DatabaseError, Exception) as error:
+        print(error)
+    return exists
+
+def check_trigger_exists(trigger_name):
+    exists = False
+    command = """select exists (select trigger_name FROM information_schema.triggers where trigger_name = '{trigger_name}');""".format(trigger_name=trigger_name)
+    try:
+        with psycopg2.connect(**Config.DATABASE) as conn:
+            with conn.cursor() as cur:
+                # execute the CREATE TABLE statement
+                cur.execute(command)
+                exists = bool(cur.fetchone()[0])
+    except (psycopg2.DatabaseError, Exception) as error:
+        print(error)
+    return exists
