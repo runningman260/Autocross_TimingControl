1c34236f9f4f95ff060457754acef40adfd65771
Nick <runningman260@users.noreply.github.com>
2024-08-31 12:31:22 -0400

    Updated App with popups and botton colors

--- CarRegistrationApp/main.py
+++ CarRegistrationApp/main.py
@@ -1,4 +1,4 @@
-#/bin/python3
+#!/usr/bin/python3
 
 #######################################################################################
 #                                                                                     #
@@ -17,14 +17,15 @@ from datetime import datetime
 import json
 
 class MQTTHandler:
-
     def create_mqtt_connection(self):
         def on_connect(client, userdata, flags, reason_code, properties):
             if reason_code == 0:
                 print("MQTT Client Connected")
             else:
                 print("MQTT Client NOT Connected, rc= " + str(reason_code))
-            client.subscribe("/timing/carreg/confirm") #This goes here to sub on reconnection
+            client.subscribe("/timing/carreg/confirm_insert") #This goes here to sub on reconnection
+            client.subscribe("/timing/carreg/confirm_update") #This goes here to sub on reconnection
+            client.subscribe("/timing/carreg/failed") #This goes here to sub on reconnection
         client = paho.Client(paho.CallbackAPIVersion.VERSION2,client_id=self.client_id)
         client.username_pw_set(Config.MQTTUSERNAME, Config.MQTTPASSWORD)
         client.on_connect = on_connect
@@ -33,24 +34,30 @@ class MQTTHandler:
         
     def subHandler(self, client, userdata, msg):
         print("Message Received")
-        if(msg.topic == "/timing/carreg/confirm"):
-            decoded_message = str(msg.payload.decode("utf-8"))
-            if(decoded_message == str(0)):
+        if((msg.topic == "/timing/carreg/failed")):
+            decoded_message = json.loads(str(msg.payload.decode("utf-8")))
+            if(decoded_message["success"] == False):
                 print("Radio for Nick Hills!")
-                # MainWindow.show_popup(self)
-                # Create an instance of the popup window and show it
+                popup = getNickPopUpWindow()
+                popup.exec()
+        if((msg.topic == "/timing/carreg/confirm_insert") or (msg.topic == "/timing/carreg/confirm_update")):
+            decoded_message = json.loads(str(msg.payload.decode("utf-8")))
+            if(decoded_message["success"] == False):
+                print("Radio for Nick Hills!")
                 popup = getNickPopUpWindow()
                 popup.exec()  # Use exec() for modal behavior
-            #else:
+            else:
                 # Clear the form when there is a successful submission
-                # How do we call this with "self" when the client is the "self"?
-            #    clearButtonClicked(self)
+                print("Successful Submit")
+                self.mainWindow.tagScanBox.setText("")
+                self.mainWindow.carNumberBox.setText("")
+                self.mainWindow.teamNameComboBox.clear()
+                self.mainWindow.teamNameComboBox.addItems(self.mainWindow.teamNameList)
             print(decoded_message)
 
     def __init__(self, mainWindow):
         super().__init__()
 
-        self.getNick = False
         self.mainWindow=mainWindow
         self.client_id = Config.CarRegistrationApp.MQTTCLIENTID
         self.client = self.create_mqtt_connection()
@@ -61,8 +68,8 @@ class MainWindow(QMainWindow):
     
     def __init__(self):
         super().__init__()
-        # Commented out to not fail when testing
-        #self.mqttClient = MQTTHandler(mainWindow=self)
+        # Comment out to not fail when testing
+        self.mqttClient = MQTTHandler(mainWindow=self)
         self.teamNameList = [""]
         self.carNumberList= [""]
         self.getCSVData()
@@ -73,46 +80,37 @@ class MainWindow(QMainWindow):
         #create input boxes
         self.tagScanLabel = QLabel("Tag Data")
         self.tagScanBox = QLineEdit()
+        self.tagScanBox.setStyleSheet("QLineEdit { margin-bottom: 20px; }")
 
         self.carNumberLabel = QLabel("Car Number")
         self.carNumberBox = QLineEdit()
+        self.carNumberBox.setStyleSheet("QLineEdit { margin-bottom: 20px; }")
         self.teamNameLabel = QLabel("Team Name")
-        #self.teamNameBox = QLineEdit()
-        # self.tagNumberLabel = QLabel("Tag Number")
-        # self.tagNumberBox = QLineEdit()
-        # self.scanTimeLabel = QLabel("Scan Time")
-        # self.scanTimeBox = QLineEdit()
 
         self.teamNameComboBox=QComboBox()
         self.teamNameComboBox.addItems(self.teamNameList)
+        self.teamNameComboBox.setStyleSheet("QComboBox { margin-bottom: 20px; }")
         self.teamNameComboBox.currentIndexChanged.connect(self.schoolChanged)
         
-
         #create button
         self.submitButton = QPushButton(text="Submit", parent=self)
-        self.submitButton.setStyleSheet('margin: 20px')
-        self.submitButton.setStyleSheet('padding: 5px')
+        self.submitButton.setStyleSheet("QPushButton { padding: 15px; margin-bottom: 10px; background-color: green; }")
         self.submitButton.clicked.connect(self.submitButtonClicked)
         
         self.clearButton = QPushButton(text="Clear", parent=self)
-        self.clearButton.setStyleSheet('margin: 20px')
-        self.clearButton.setStyleSheet('padding: 5px')
+        self.clearButton.setStyleSheet("background-color: red")
         self.clearButton.clicked.connect(self.clearButtonClicked)
 
         #create layout
         self.layout = QVBoxLayout()
+        #self.layout.setSpacing(20)
         self.layout.addWidget(self.tagScanLabel)
         self.layout.addWidget(self.tagScanBox)
-        # self.layout.addWidget(QLabel("----------OR----------"))
 
         self.layout.addWidget(self.carNumberLabel)
         self.layout.addWidget(self.carNumberBox)
         self.layout.addWidget(self.teamNameLabel)
         self.layout.addWidget(self.teamNameComboBox)
-        # self.layout.addWidget(self.tagNumberLabel)
-        # self.layout.addWidget(self.tagNumberBox)
-        # self.layout.addWidget(self.scanTimeLabel)
-        # self.layout.addWidget(self.scanTimeBox)
         self.layout.addWidget(self.submitButton)
         self.layout.addWidget(self.clearButton)
 
@@ -124,15 +122,20 @@ class MainWindow(QMainWindow):
 
     def submitButtonClicked(self):
         print("Submit Button Pressed")
-        print(self.tagScanBox.text())
-        data = {
-            "car_number"    :   self.carNumberBox.text(),
-            "team_name"     :   self.teamNameComboBox.currentText(),
-            "tag_number"    :   self.tagScanBox.text(),
-            "scan_time"     :   datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
-        }
-        data_json = json.dumps(data, indent=4)
-        self.mqttClient.client.publish("/timing/carreg/newcar", payload=data_json, qos=0)
+        if(self.tagScanBox.text() == "" or self.carNumberBox.text() == ""):
+            print("Blank Fields Found, not submitting")
+            popup = blankFieldsPopUpWindow()
+            popup.exec()
+        else:
+            print(self.tagScanBox.text())
+            data = {
+                "car_number"    :   self.carNumberBox.text(),
+                "team_name"     :   self.teamNameComboBox.currentText(),
+                "tag_number"    :   self.tagScanBox.text(),
+                "scan_time"     :   datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+            }
+            data_json = json.dumps(data, indent=4)
+            self.mqttClient.client.publish("/timing/carreg/newcar", payload=data_json, qos=0)
     
     def clearButtonClicked(self):
         print("Clear Button Pressed")
@@ -151,40 +154,42 @@ class MainWindow(QMainWindow):
     def schoolChanged(self):
         self.carNumberBox.setText(self.carNumberList[self.teamNameComboBox.currentIndex()])
 
-    @staticmethod
-    def show_popup(self):
-        # Create a QMessageBox object
-        msg = QMessageBox()
-
-        # Set the title and message
-        msg.setWindowTitle("Popup Window")
-        msg.setText("This is a popup message!")
-
-        # Add buttons to the popup
-        msg.setStandardButtons(QMessageBox.StandardButton.Ok)
-
-        # Show the popup window
-        msg.exec()
-
 class getNickPopUpWindow(QDialog):
     def __init__(self):
         super().__init__()
 
         # Set the title and size of the popup
-        self.setWindowTitle("Popup Window")
-        self.setGeometry(100, 100, 200, 100)
+        self.setWindowTitle("Team Submission FAILED")
+        self.setGeometry(100, 100, 400, 400)
+        self.setStyleSheet('font-size: ' + str(48)+'px')
 
         # Add a label to the popup
         layout = QVBoxLayout()
-        label = QLabel("Radio for Nick Hills!", self)
+        label = QLabel("Something didn't work!\nRadio for Nick Hills!", self)
+        label.setAlignment(Qt.AlignmentFlag.AlignCenter)
         layout.addWidget(label)
         self.setLayout(layout)
 
-getNick = False
+class blankFieldsPopUpWindow(QDialog):
+    def __init__(self):
+        super().__init__()
+
+        # Set the title and size of the popup
+        self.setWindowTitle("Blanks Found")
+        self.setGeometry(100, 100, 400, 400)
+        self.setStyleSheet('font-size: ' + str(48)+'px')
+
+        # Add a label to the popup
+        layout = QVBoxLayout()
+        label = QLabel("Blank Rows found\nNo submission.", self)
+        label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        layout.addWidget(label)
+        self.setLayout(layout)
 
 app = QApplication(sys.argv)
 
 window = MainWindow()
+window.setMinimumWidth(800)
 window.show()
 
 app.exec()
