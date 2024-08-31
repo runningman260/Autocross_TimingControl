64b54947f225299a4a115052dec4113031116928
Nick <runningman260@users.noreply.github.com>
2024-08-31 13:38:18 -0400

    Forgot to add files to last commit

---
+++ Initialization/config.py
@@ -0,0 +1,22 @@
+#/bin/python3
+
+##################################################################
+#                                                                #
+#  SchemaMgmt Configuration class                                #
+#                                                                #
+####################################### Pittsburgh Shootout LLC ##
+
+class Config:
+    class MQTT:
+        PORT = 1883
+        USERNAME = 'username'
+        PASSWORD = 'password'
+        BROKER = '192.168.2.200'
+        CLIENTID = 'SchemaMgmt'
+        TESTERCLIENTID = 'SchemaMgmtTester'
+    
+    class DB:
+        HOST="192.168.2.200"
+        NAME="test_scans"
+        USER="nick"
+        PASS="password"
