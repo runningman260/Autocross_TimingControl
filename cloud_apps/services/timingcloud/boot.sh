1d9787608f0d30463550a1dbfeaa445c7e5b8c13
Jake <Goot449@users.noreply.github.com>
2025-07-14 23:35:57 -0400

    tweaks to cloudui before major rework for other events

--- cloud_apps/services/timingcloud/boot.sh
+++ cloud_apps/services/timingcloud/boot.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # this script is used to boot a Docker container
-exec gunicorn -b :5000 --access-logfile - --error-logfile - timingcloud:app
+exec gunicorn -b :5000 --workers=3 --access-logfile - --error-logfile - timingcloud:app
