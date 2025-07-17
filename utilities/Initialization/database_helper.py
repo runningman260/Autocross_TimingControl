178509e44ee6b2502d6cf6b58ac5300b49c7a10d
Jake <Goot449@users.noreply.github.com>
2025-07-17 13:34:18 -0400

    revert db changes for easier merge with actual changes

--- utilities/Initialization/database_helper.py
+++ utilities/Initialization/database_helper.py
@@ -743,9 +743,7 @@ def insert_teams():
 		("York College of Pennsylvania YC Racing", "YCR"),
 		("Liberty University Flames Racing", "LUFR"),
 		("Carnegie Mellon Racing", "CMR"),
-		("Kennesaw State University Kennesaw Motorsports", "KSUM"),
-		("University of California Irvine ", "UCIRV"),
-		("West Virginia Mountaineer Racing", "WVU")
+		("Kennesaw State University Kennesaw Motorsports", "KSUM")
 	]
 	try:
 		with psycopg2.connect(
